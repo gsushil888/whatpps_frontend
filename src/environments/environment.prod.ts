52bf69c487d21b490e3047b3a7da4ff32e47f96d
gsushil888 <tmail7458@gmail.com>
2026-03-17 18:54:01 +0530

    Running Code without env and assets file new

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://api.yourapp.com/api/v1/',
-  wsUrl: 'wss://api.yourapp.com/ws',
-  rtc: {
-    iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }]
-  }
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1/',
-    wsUrl: 'http://localhost:8080/ws',
-    rtc: {
-        iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] // add TURN in prod
-    }
-};
